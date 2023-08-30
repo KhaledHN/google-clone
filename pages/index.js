4b42fd2af36d362786ba74188f8180d639c8a330
Khaled Hazem <khaled.hazem2009@gmail.com>
2023-08-31 01:44:53 +0300

    Adding Header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Avatar from "@/components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,29 @@ export default function Home() {
         <title>Google</title>
       </Head>
 
-      <h1>Let build google with next.js and tailwind</h1>
+      {/* Header */}
+      <header className=" flex w-full p-5 justify-between text-sm text-gray-700 ">
+        {/* Left */}
+        <div className=" flex space-x-4 items-center ">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+        {/* Right */}
+        <div className=" flex space-x-4 items-center ">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+
+          {/* Icon */}
+          <ViewGridIcon className=" h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer " />
+
+          {/* Avatar */}
+          <Avatar url="https://lh3.googleusercontent.com/a/AAcHTtcdNoItMI_Enq1Er0axcEKntEvQJUyKK38e1aGFu4L-DInI=s288-c-no" />
+        </div>
+      </header>
+
+      {/* Body */}
+
+      {/* Footer */}
     </div>
   );
 }
